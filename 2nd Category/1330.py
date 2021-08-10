6451f36ae1b38891e19756c187d56ea05345d6eb
yaerhee <yaerhee@naver.com>
2021-08-10 23:53:19 +0900

    BOJ #1330(re)

--- 2nd Category/1330.py
+++ 2nd Category/1330.py
@@ -1,7 +1,12 @@
+# A, B = map(int, input().split())
+# if A > B:
+#     print('>')
+# elif A < B:
+#     print('<')
+# else:
+#     print('==')
+#
+# 삼항 연산자?
+# 조건식 1이 참일때 값 & [if 조건식 1] & 조건식 2가 참일때 값 & [if 조건식 2] & 모두 거짓일 때 값
 A, B = map(int, input().split())
-if A > B:
-    print('>')
-elif A < B:
-    print('<')
-else:
-    print('==')
+print('>') if A > B else print('<') if A < B else print('==')
